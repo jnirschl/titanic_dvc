3b8d679e5240d3acb11832ec7a6cfed9fb559ac0
Jeff Nirschl <jnirschl@stanford.edu>
2021-01-17 22:21:40 -0800

    Data cleaning to remove indices with nan values

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,38 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+#   -*- coding: utf-8 -*-
+#  Copyright (c) 2021.  Jeffrey J. Nirschl. All rights reserved.
+#
+#   Licensed under the MIT license. See the LICENSE.md file in the project
+#   root directory for full license information.
+#
+#   Time-stamp: <>
+# ======================================================================
 
+import os
+import argparse
+import pandas as pd
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def remove_nan(input_filepath, output_dir):
+    """Remove rows  with missing values from the dataset"""
+    assert (os.path.isfile(input_filepath)), FileNotFoundError
+    assert (os.path.isdir(output_dir)), NotADirectoryError
 
+    df = pd.read_csv(input_filepath, sep=",", header=0)
+
+    # drop unused cols
+    df = df.drop(columns=["Name", "Cabin"])
+    df = df.dropna(axis=0, how="any")
+
+    # save file
+    output_filename = os.path.basename(input_filepath.replace(".csv",
+                                                              "_clean.csv"))
+    output_filepath = os.path.join(output_dir, output_filename)
+    df.to_csv(output_filepath)
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--out-dir", dest="output_dir",
+                        required=True, help="output directory")
+    parser.add_argument("-i", "--input", dest="input_filepath",
+                        required=True, help="input CSV file")
+    args = parser.parse_args()
+    remove_nan(args.input_filepath, args.output_dir)
