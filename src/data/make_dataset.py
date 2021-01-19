8350d592c224de8c6d983f58eea01e34ba33d5ca
Jeff Nirschl <jnirschl@stanford.edu>
2021-01-18 21:20:00 -0800

    Update make dataset to encode categorical variables, optionally remove nan from training, and save categorized data as well as yaml encoding categorical classes

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -9,29 +9,13 @@
 
 import os
 import argparse
-import pandas as pd
 import pathlib
+import pandas as pd
+import yaml
 
-def remove_nan(input_filepath, output_dir):
-    """Remove rows  with missing values from the dataset"""
-    assert (os.path.isfile(input_filepath)), FileNotFoundError
-    assert (os.path.isdir(output_dir)), NotADirectoryError
-
-    df = pd.read_csv(input_filepath, sep=",", header=0)
-
-    # drop unused cols
-    df = df.drop(columns=["Name", "Cabin"])
-
-    # remove indices with any nan columns
-    df = df.dropna(axis=0, how="any")
-
-    # save file
-    output_filename = os.path.basename(input_filepath.replace(".csv",
-                                                              "_clean.csv"))
-    output_filepath = os.path.join(output_dir, output_filename)
-    df.to_csv(output_filepath)
-
-def categorize(train_path, test_path, output_dir):
+def encode_labels(train_path, test_path,
+                  output_dir, remove_nan=False,
+                  label_dict_name="label_encoding.yaml"):
     """Impute missing values"""
     assert (os.path.isfile(train_path)), FileNotFoundError
     assert (os.path.isfile(test_path)), FileNotFoundError
@@ -49,32 +33,59 @@ def categorize(train_path, test_path, output_dir):
     # drop unnecessary filenames
     df = df.drop(columns=["Name", "Cabin", "Ticket"])
 
-    # set categorical features
-    df["EmbarkedId"] = df["Embarked"].astype("category").cat.codes
-    df["Sex"] = df["Sex"].astype("category").cat.codes
+    # convert to categorical
+    df["Embarked"] = df["Embarked"].astype("category")
+    df["Sex"] = df["Sex"].astype("category")
 
-    # set output columns
-    cols = [*df.columns, "EmbarkedId"]
+    # save dictionary mapping text to categorical label
+    embarked_dict = {key: val for key, val in enumerate(df["Embarked"].cat.categories)}
+    sex_dict = {key: val for key, val in enumerate(df["Sex"].cat.categories)}
 
-    # save file
+    # transform to categorical codes
+    df["Embarked"] = df["Embarked"].cat.codes
+    df["Sex"] = df["Sex"].cat.codes
+
+    # return datasets to train and test
+    train_df = df.loc[train_df.index, df.columns]
+    test_df = df.loc[test_df.index, df.columns[1:]]
+
+    # remove nan (if applicable
+    if remove_nan:
+        train_df = train_df.dropna(axis=0, how="any")
+
+    # set output and save filenames
     output_dir = pathlib.Path(output_dir)
     save_train_fname = os.path.basename(train_path.replace(".csv", "_categorized.csv"))
     save_test_fname = os.path.basename(test_path.replace(".csv", "_categorized.csv"))
-    df.loc[train_df.index, cols].to_csv(output_dir.joinpath(save_train_fname))
-    df.loc[test_df.index, cols[1:]].to_csv(output_dir.joinpath(save_test_fname)) # test cols starts from 1 because survival status is hidden
+
+    # save updated dataframes
+    train_df.to_csv(output_dir.joinpath(save_train_fname))
+    test_df.to_csv(output_dir.joinpath(save_test_fname))  # test cols starts from 1 because survival status is hidden
+
+    # save and encoding dictionaries
+    encoding_dict = yaml.safe_dump({"Embarked": embarked_dict,
+                                    "Sex": sex_dict})
+    with open(os.path.join(output_dir, label_dict_name), "w") as writer:
+        writer.writelines(encoding_dict)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--out-dir", dest="output_dir",
                         required=True, help="output directory")
-    parser.add_argument("-r", "--train", dest="train_path",
+    parser.add_argument("-tr", "--train", dest="train_path",
                         required=True, help="Train CSV file")
-    parser.add_argument("-e", "--test", dest="test_path",
+    parser.add_argument("-te", "--test", dest="test_path",
                         required=True, help="Test CSV file")
+    parser.add_argument("-r", "--remove-nan", dest="remove_nan",
+                        default=False, required=False,
+                        help="Remove nan rows from training dataset")
+    parser.add_argument("-l", "--label", dest="label_dict_name",
+                        default="label_encoding.yaml",
+                        required=False, help="Name for dictionary mapping category codes to text")
     args = parser.parse_args()
-    #remove_nan(args.input_filepath, args.output_dir)
 
     # convert categorical variables into integer codes
-    categorize(args.train_path, args.test_path,
-                   args.output_dir)
+    encode_labels(args.train_path, args.test_path, args.output_dir,
+                  remove_nan=args.remove_nan,
+                  label_dict_name=args.label_dict_name)
